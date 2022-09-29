f4fbd45a013196105d43bcecc59393edaac9c1ee
jakeHaln <JakeHaln@protonmail.com>
2022-09-29 14:12:10 +0800

    change name

--- white-list/pages/index.js
+++ white-list/pages/index.js
@@ -1,102 +1,103 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Web3Modal from 'web3modal'
-import { providers, Contract } from 'ethers'
-import { useEffect, useRef, useState } from 'react'
-import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 export default function Home() {
-  const [walletConnected, setWalletConnected] = useState(false)
+  const [walletConnected, setWalletConnected] = useState(false);
 
-  const [joinedWhitelist, setJoinedWhitelist] = useState(false)
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0)
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
 
-  const web3ModalRef = useRef()
+  const web3ModalRef = useRef();
 
   const getProviderOrSigner = async (needSigner = false) => {
-    const provider = await web3ModalRef.current.connect()
-    const web3Provider = new providers.Web3Provider(provider)
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
 
-    const { chainId } = await web3Provider.getNetwork()
+    const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 4) {
-      window.alert('Change the network to Rinkeby')
-      throw new Error('Change network to Rinkeby')
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
     }
 
     if (needSigner) {
-      const signer = web3Provider.getSigner()
-      return signer
+      const signer = web3Provider.getSigner();
+      return signer;
     }
-    return web3Provider
-  }
+    return web3Provider;
+  };
   const addAddressToWhitelist = async () => {
     try {
-      const signer = await getProviderOrSigner(true)
+      const signer = await getProviderOrSigner(true);
 
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
-        signer,
-      )
-      const tx = await whitelistContract.addAddressToWhitelist()
-      setLoading(true)
+        signer
+      );
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setLoading(true);
 
-      await tx.wait()
-      setLoading(false)
-      await getNumberOfWhitelisted()
-      setJoinedWhitelist(true)
+      await tx.wait();
+      setLoading(false);
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const getNumberOfWhitelisted = async () => {
     try {
-      const provider = await getProviderOrSigner()
+      const provider = await getProviderOrSigner();
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
-        provider,
-      )
-      const _numberOfWhitelisted = await whitelistContract.numAddressesWhitelisted()
-      setNumberOfWhitelisted(_numberOfWhitelisted)
+        provider
+      );
+      const _numberOfWhitelisted =
+        await whitelistContract.numAddressesWhitelisted();
+      setNumberOfWhitelisted(_numberOfWhitelisted);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const checkIfAddressInWhitelist = async () => {
     try {
-      const signer = await getProviderOrSigner(true)
+      const signer = await getProviderOrSigner(true);
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
-        signer,
-      )
-      const address = await signer.getAddress()
+        signer
+      );
+      const address = await signer.getAddress();
       const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
-        address,
-      )
-      setJoinedWhitelist(_joinedWhitelist)
+        address
+      );
+      setJoinedWhitelist(_joinedWhitelist);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
-      await getProviderOrSigner()
-      setWalletConnected(true)
+      await getProviderOrSigner();
+      setWalletConnected(true);
 
-      checkIfAddressInWhitelist()
-      getNumberOfWhitelisted()
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const renderButton = () => {
     if (walletConnected) {
@@ -105,35 +106,35 @@ export default function Home() {
           <div className={styles.description}>
             Thanks for joining the Whitelist!
           </div>
-        )
+        );
       } else if (loading) {
-        return <button className={styles.button}>Loading...</button>
+        return <button className={styles.button}>Loading...</button>;
       } else {
         return (
           <button onClick={addAddressToWhitelist} className={styles.button}>
             Join the Whitelist
           </button>
-        )
+        );
       }
     } else {
       return (
         <button onClick={connectWallet} className={styles.button}>
           Connect your wallet
         </button>
-      )
+      );
     }
-  }
+  };
 
   useEffect(() => {
     if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
-        network: 'rinkeby',
+        network: "rinkeby",
         providerOptions: {},
         disableInjectedProvider: false,
-      })
-      connectWallet()
+      });
+      connectWallet();
     }
-  }, [walletConnected])
+  }, [walletConnected]);
 
   return (
     <div>
@@ -158,9 +159,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
+      <footer className={styles.footer}>Made with &#10084; by Sayadi</footer>
     </div>
-  )
+  );
 }
